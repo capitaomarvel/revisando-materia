6c98f2c6b50be63ea78ffb498834552e87ab1c61
capitaomarvel <claraoliveiradasilva53@gmail.com>
2024-11-25 07:49:46 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "Portugues",
+    "Quais são as eras do romantismo no Brasil?",
+    "Primeira geração: nacionalista, indianista e religiosa Segunda geraçãogeração do mal-do-século ou Ultrarromântica Terceira geração: geração condoreira"
+)
+
+criaCartao(
+    "Tecnologia",
+    "O que é GiHub",
+    "A plataforma com um banco de armazenamento com códigos de programação, além de auxiliar os programadores em seus projetos, também é uma rede de compartilhamento e troca de conhecimento entre os profissionais dessa área."
+)
+
+criaCartao(
+    "Biotecnologia",
+    "Como a técnica de CRISPR-Cas9 tem sido utilizada no desenvolvimento de terapias genéticas para doenças hereditárias?,
+    "A técnica de **CRISPR-Cas9** é usada para corrigir mutações causadoras de doenças hereditárias ao editar genes diretamente no DNA. Primeiro, identifica-se o gene defeituoso. Depois, um RNA guia direciona a proteína Cas9 ao local específico no genoma, onde ela faz um corte. Esse corte é reparado pela célula, permitindo a substituição ou correção do gene defeituoso. Aplicações incluem o tratamento de condições como anemia falciforme, distrofia muscular e fibrose cística. A técnica oferece precisão e personalização, embora ainda enfrente desafios como a garantia de segurança e eficácia."
+)
